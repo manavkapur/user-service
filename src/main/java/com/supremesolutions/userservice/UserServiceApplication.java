054345f6495cf15c4acf849fc64791361c7f359a
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2025-10-29 21:07:48 +0530

    Update UserServiceApplication.java

--- src/main/java/com/supremesolutions/userservice/UserServiceApplication.java
+++ src/main/java/com/supremesolutions/userservice/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.supremesolutions.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
